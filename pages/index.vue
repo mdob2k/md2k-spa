9cc0acbd87650871d52198f7b87cca5b45471c83
mdob2k <mdob2k@gmail.com>
2019-03-10 11:11:43 +0100

    update dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,17 @@
   <div class="container has-text-centered">
     <h1 class="title">md2k-spa</h1>
     <h2 class="subtitle">My kryptonian Nuxt.js project</h2>
+    <logo/>
     <div class="links">
       <a
+        class="button--green"
         href="https://nuxtjs.org/"
         target="_blank"
-        class="button--green"
       >Documentation</a>
       <a
+        class="button--grey"
         href="https://github.com/nuxt/nuxt.js"
         target="_blank"
-        class="button--grey"
       >GitHub</a>
     </div>
   </div>
