12458d8b3724ae0ecc2b06e789ee53b67e9f2a1a
mdob2k <mdob2k@gmail.com>
2019-05-18 21:41:59 +0200

    Update depedencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,13 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    'plugin:vue/recommended'
-  ],
+  extends: ['plugin:vue/recommended'],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/singleline-html-element-content-newline': 'off'
   }
 }
